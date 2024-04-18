c1a3011206560cc246ec722eff1b618d60f1708b
Caue <cauusaantos@gmail.com>
2024-04-17 22:24:54 -0300

    focused

---
+++ Aplicacoes/ex8.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você Ultrapassou a velocidade Permitida, MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
