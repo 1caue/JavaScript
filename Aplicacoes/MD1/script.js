fbaa84b71a67bfcb3eeb480edf51c275bc17dbb8
Caue <cauusaantos@gmail.com>
2024-04-22 22:19:47 -0300

    test

--- Aplicacoes/MD1/script.js
+++ Aplicacoes/MD1/script.js
@@ -1,16 +1,20 @@
-function carregar() {
+function carregar() { 
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora < 12) {
-        // meio dia
-    } else if (hora >= 12 && hora < 18) {
-    // boa tarde
+    msg.innerHTML = `Agora são <strong>${hora} Horas</strong>`
+    if (hora >= 0 && hora <= 12) {
+        // bom dia 
+        img.src = 'manha.png'
+        document.body.style.background = '#c9cdd8'
+    } else if (hora > 12 && hora <= 18) {
+        // boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#aed2ec'
     } else {
-    console.log('Boa noite')
-    // boa noite
+        // boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#181510'
     }
 }
-
