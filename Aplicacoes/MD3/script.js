5a462e5445088be10995eb1538914ddcc0d35f2a
Caue <cauusaantos@gmail.com>
2024-05-01 20:30:05 -0300

    test

--- Aplicacoes/MD3/script.js
+++ Aplicacoes/MD3/script.js
@@ -1,5 +1,19 @@
-var b = document.getElementById('ini')
-var c
-for (var a = 1; a <= b; a += c) {
-
+function button() {
+    var inicio = document.getElementById('ini')
+    var passo = document.getElementById('pas')
+    var fim = document.getElementById('fim')
+    var res = document.getElementById('res')
+    
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        window.alert('[Erro] Faltam dados')
+    } else {
+        res.innerHTML = `<strong>Contando: </strong>`
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        
+        for(var c = i; c <= f; c += p) {
+            res.innerHTML += `<strong>${c}</strong>&#128073; `
+        }
+    }
 }
