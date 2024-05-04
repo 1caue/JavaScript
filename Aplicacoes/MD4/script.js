fd87846c3a816f8e7293e49aa824a959335aafa3
Caue <cauusaantos@gmail.com>
2024-05-04 20:39:22 -0300

    test

--- Aplicacoes/MD4/script.js
+++ Aplicacoes/MD4/script.js
@@ -1,12 +1,15 @@
 function tabuada() {
     let num = document.getElementById("txtn")
     let tab = document.getElementById("seltab")
+    
     if (num.value.lenght == 0) {
         window.alert('Por Favor, Digite os valores!')
+    
     } else {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
+    
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n * c}`
