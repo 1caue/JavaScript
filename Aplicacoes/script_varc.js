3b0f1bf7b81e8988b6cc77a8a3ba2f272dff61ea
Caue <cauusaantos@gmail.com>
2024-05-28 22:07:31 -0300

    test

--- Aplicacoes/script_varc.js
+++ Aplicacoes/script_varc.js
@@ -1,2 +1,8 @@
-let num = [5, 8, 2, 9, 3]
-console.log(`O nosso vetor é o ${num}`)
+let num = [5, 1, 2, 4, 3]
+/*console.log(`O nosso vetor é o ${num.sort()}`)
+console.log(`O primeiro valor do vetor é o ${num[0]}`)
+console.log(`O nosso maior valor é o ${num.length}`)
+console.log(num)*/
+for(let pos=0; pos<num.length; pos++) {
+    console.log(`A posição ${pos+1} tem o valor ${num[pos]}`)
+}
