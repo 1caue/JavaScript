ff0b9da5aea6f74a04ffa0e3b57f5e3cae9075f5
Caue <cauusaantos@gmail.com>
2024-06-10 11:59:29 -0300

    JS

--- Node/index.js
+++ Node/index.js
@@ -4,17 +4,14 @@ const path = require('path');
 const router = express.Router();
 
 
-require.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'index.html'))
+router.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    })
 
-})
-
-router.get('/sobre ')
-
-
-
-
-
-router.get('A', router)
-app.use('/', router)
-app.listen(process.env.port || 3000)
+router.get('/sobre', function(req,res){
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+    })
+    
+app.use('/',router);
+app.listen(process.env.port || 3000);
+console.log('Servidor Rodando!');
