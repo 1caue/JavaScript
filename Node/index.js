668269cc0065424c35139582d7ce458b0b980cff
Caue <cauusaantos@gmail.com>
2024-06-14 11:59:59 -0300

    test

--- Node/index.js
+++ Node/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const bodyParser = require('body-parser')
+
+var login = 'admin'
+var password = '123456'
 
 router.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
@@ -11,10 +15,32 @@ router.get('/sobre', function(req,res){
     res.sendFile(path.join(__dirname+'/sobre.html'));
     })
 
-router.get('/mais', function(req,res){
-    res.sendFile(path.join(__dirname+'/mais.html'));
+router.get('/login', function(req,res){
+    res.sendFile(path.join(__dirname+'/login.html'));
     })
 
+app.post('/', (req, res)=> {
+    
+    if(req.body.password == password && req.body.login == login){
+        //Logado com Sucesso!
+        req.session.login = login;
+        res.redirect('/');
+    }
+})
+
+app.get('/', (req, res) =>{
+    if(req, session.login){
+        res.render('Logado!');
+    }else{
+        res.render('index');
+    }
+})
+
+app.listen(port, ()=>{
+    console.log('Função rodando!');
+})
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',router);
 app.listen(process.env.port || 3000);
 console.log('Servidor Rodando!, Para parar basta Dar um "Ctrl + C"');
