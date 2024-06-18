6d09aa12e30b98701c1a4fdbfa759d5820a1f528
Caue <cauusaantos@gmail.com>
2024-06-18 12:05:41 -0300

    Uploads

--- Node/API/index.js
+++ Node/API/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const server = express();
+const filmes = require('/scr/data/filmes.json');
 
-server.get('/', (req,res) => {
-    return res.json({usuário: 'Master 1'})
+server.get('/filmes', (req,res) => {
+    return res.json({filmes})
 });
 
 server.listen(3000, () =>{
-    console.log('Servidor está Funcionando... Para parar-ló digite CTRL+C')
+    console.log('Servidor está Funcionando...Para parar-ló digite CTRL+C')
 });
