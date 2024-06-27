ac6ed10b80c919462332b41f801ce9cac1a71ee3
Caue <cauusaantos@gmail.com>
2024-06-27 12:04:48 -0300

    Updates

--- Node/LocalAplication/script.js
+++ Node/LocalAplication/script.js
@@ -1,11 +1,9 @@
 var readline = require('readline-sync');
 var nome = readline.question("Qual seu nome?: ");
-var Idade = readline.questionInt("Qual a sua idade?: ");
-var pai = readline.question("Qual o nome do seu pai?: ");
-var mae = readline.question("Qual o nome da sua mae?: ");
+var n1 = readline.questionInt(`Insira o primeiro valor: `);
+var n2 = readline.questionInt(`Insira o segundo valor: `);
+var s = n1 + n2
 
 console.log('------------------------');
-console.log(`O nome inserido pelo usuário foi ${nome}`);
-console.log(`A Idade inserida pelo usuário foi ${Idade}`);
-console.log(`O nome do pai do usuário é ${pai}`);
-console.log(`O nome da mãe do usuário é ${mae}`);
+console.log(`${nome} a Soma dos dois valores é:`);
+console.log(`${n1} + ${n1} = ${s}`);
