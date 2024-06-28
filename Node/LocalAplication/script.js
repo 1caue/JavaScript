3c5a82f8362e60ce40df5f87fceeca16cbeab04a
Caue <cauusaantos@gmail.com>
2024-06-28 12:06:00 -0300

    Updates

--- Node/LocalAplication/script.js
+++
@@ -1,9 +0,0 @@
-var readline = require('readline-sync');
-var nome = readline.question("Qual seu nome?: ");
-var n1 = readline.questionInt(`Insira o primeiro valor: `);
-var n2 = readline.questionInt(`Insira o segundo valor: `);
-var s = n1 + n2
-
-console.log('------------------------');
-console.log(`${nome} a Soma dos dois valores é:`);
-console.log(`${n1} + ${n1} = ${s}`);
