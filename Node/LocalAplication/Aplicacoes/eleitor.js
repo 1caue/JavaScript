57187a8c89e1211a0f4e2644f8c332c829d758d6
Caue <cauusaantos@gmail.com>
2024-06-29 17:18:46 -0300

    Updates

---
+++ Node/LocalAplication/Aplicacoes/eleitor.js
@@ -0,0 +1,12 @@
+var readline = require('readline-sync');
+var name = readline.question('Qual o seu nome?: ');
+var idade = readline.questionInt('Quantos anos voce tem?: ');
+
+console.log('---------------------------------------')
+if (idade < 16) {
+    console.log(`${name} você tem ${idade} anos, você ainda NÃO pode votar`)
+} else if (idade == 16 || idade > 65) {
+    console.log(`${name} você tem ${idade} anos, você PODE votar porém NÃO é OBRIGADO`)
+} else {
+    console.log(`${name} você tem ${idade} anos, você é OBRIGADO a votar`)
+}
