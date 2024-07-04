450c6b7b8e7804b9c6b9abcf8711ecc8819301bf
Caue <cauusaantos@gmail.com>
2024-07-04 18:08:13 -0300

    Final Update

--- Node/LocalAplication/Aplicacoes/conv.js
+++ Node/LocalAplication/Aplicacoes/conv.js
@@ -4,4 +4,4 @@ let color = require('colors');
 console.log('-------- CONVERSOR --------'.bgGreen);
 let v = re.questionInt('Digite o valor que deseja converter em reais: ');
 var d = v / 5.68
-console.log(`R$${v} = U$${d}`)
+console.log(`R$${v} = U$${d}`.bgBlue)
