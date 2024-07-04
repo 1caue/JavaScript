7331e7e0a5816cdd4ab54bcc45a8e9dc85761054
Caue <cauusaantos@gmail.com>
2024-07-04 18:08:41 -0300

    Update

---
+++ Node/LocalAplication/Aplicacoes/tst.js
@@ -0,0 +1,25 @@
+console.clear();
+let read = require('readline-sync');
+let cor = require('colors');
+
+let guess = 'S';
+
+while ( guess === 'S' || guess === 's') {
+    
+    console.log('-------- CONVERSOR --------'.bgGreen);
+    let v = read.questionInt('Digite o valor que deseja converter em reais: ');
+    let d = v / 5.68
+    console.log(`R$${v} = U$${d.toFixed(2)}`.bgBlue)
+    
+    guess = read.question('Deseja continuar? [S/N]: ');
+    
+    if (guess === 'N' || guess === 'n') {
+        break;   
+    
+    } else if ( guess !== 'S' && guess !== 's' ) 
+    
+    { console.log('Por favor digite Apenas "S" para sim e "N" para nao e tente novamente'.bgRed);
+    
+    }
+}
+    
