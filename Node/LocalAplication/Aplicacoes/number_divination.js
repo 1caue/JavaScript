1626d66360150f9e71209126b6cb0c7280bc2a43
Caue <cauusaantos@gmail.com>
2024-07-11 18:11:52 -0300

    Update

--- Node/LocalAplication/Aplicacoes/number_divination.js
+++ Node/LocalAplication/Aplicacoes/number_divination.js
@@ -1,13 +1,23 @@
 var read = require('readline-sync');
 var color = require('colors');
-var random = Math.floor(Math.random() * 5); // sorteador de valores
-var question = read.question('Advinhe um valor de 0 a 5: ');
+var cont = 0
+var conterr = 0
 
-if (question == random) {
-    console.log('Parabéns voce acertou!');
-    console.log(`O Computador inseriu ${random}`.green)
-} else {
-
+while (true) {
+    var question = read.questionInt('Advinhe um valor de 0 a 5: ');
+    var random = Number(Math.floor(Math.random() * 5)); // sorteador de valores
+    cont ++
+    if (question == random) {
+        console.clear();
+        console.log('Parabéns voce acertou!');
+        console.log(`O Computador inseriu ${random}`.green);
+        console.log(`TOTAL DE TENTATIVAS: ${cont}`.gray);
+        console.log(`TOTAL DE ERROS: ${conterr}`.red);
+        break;
+ 
+    } else {
+        console.log('Não foi dessa vez, tente novamente!'.red);
+        console.log(`O Computador inseriu ${random}`.green);
+        conterr++
+    }
 }
-
-console.log(random)
