4c113af1e0072af4eb32c42a825298bf786ccd62
Caue <cauusaantos@gmail.com>
2024-07-30 18:08:36 -0300

    Root

--- Angular/Ajax/root.js
+++ Angular/Ajax/root.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 const contatos = [
@@ -35,14 +36,61 @@ const contatos = [
         }
 ];
 
+const operadoras = [
+    {
+        "nome": "OI",
+        "codigo": 14,
+        "categoria": "Celular",
+        "operadora": 2
+    },
+    {
+        "nome": "Vivo",
+        "codigo": 15,
+        "categoria": "Celular",
+        "operadora": 1
+    },
+    {
+        "nome": "Tim",
+        "codigo": 41,
+        "categoria": "Celular",
+        "operadora": 3
+    },
+    {
+        "nome": "GVT",
+        "codigo": 25,
+        "categoria": "Fixo",
+        "operadora": 1
+    },
+    {
+        "nome": "Embratel",
+        "codigo": 21,
+        "categoria": "Fixo",
+        "operadora": 2
+    }
+];
+
 app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/operadoras', (req, res) => {
+    res.json(operadoras);
+});
 
 app.get('/contatos', (req, res) => {
     res.json(contatos);
+});
+
+app.post('/contatos', (req, res) => {
+    const contato = req.body;
+    contatos.push(contato);
+    res.status(201).json(contato);
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log('LINK: http://127.0.0.1:5500/Ajax/ajax.html');
+    console.log('LINK CONTATOS: http://localhost:3000/contatos');
+    console.log('LINK OPERADORAS: http://localhost:3000/operadoras');
 });
+
