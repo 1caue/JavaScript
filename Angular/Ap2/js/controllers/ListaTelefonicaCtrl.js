9288c4214bececa17e517ccccc36357ebd40c17d
Caue <cauusaantos@gmail.com>
2024-08-07 13:11:57 -0300

    Working

---
+++ Angular/Ap2/js/controllers/ListaTelefonicaCtrl.js
@@ -0,0 +1,46 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, uppercaseFilter, $http) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [];
+    $scope.operadoras = [];
+    
+    $scope.adicionarContato = function (contato) {
+        console.log("Adicionando contato:", contato);
+        $http.post("http://localhost:3000/contatos", contato).then(function (response) {
+            console.log("Resposta do servidor:", response.data);
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
+            carregarContatos();
+        });
+    };
+
+    var carregarContatos = function () {
+        $http.get('http://localhost:3000/contatos').then(function (response) {
+            $scope.contatos = response.data;
+            console.log("Contatos carregados:", response.data);
+        })
+    };
+
+    var carregarOperadoras = function () {
+        $http.get("http://localhost:3000/operadoras").then(function (response, data) {
+            $scope.operadoras = response.data;
+            console.log("Operadoras carregadas: ", response.data);
+        });
+    };
+
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+           if (!contato.selecionado) return contato;
+        });
+    };
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+carregarContatos();
+carregarOperadoras();
+});
