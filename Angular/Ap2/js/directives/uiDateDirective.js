fedec63aeb06b6600b122082e6ba8921de8fc25f
Caue <cauusaantos@gmail.com>
2024-08-22 17:17:06 -0300

    Update

--- Angular/Ap2/js/directives/uiDateDirective.js
+++ Angular/Ap2/js/directives/uiDateDirective.js
@@ -1,7 +1,11 @@
 angular.module("listaTelefonica").directive("uiDate", function () {
     return {
-        link: function () {
-            console.log("uiDate");
+        require: "ngModel",
+        link: function (scope, element, attrs, ctrl) {
+            element.bind("keyup", function () {
+                console.log(ctrl.$viewValue);
+
+            });
         }
     };
 });
