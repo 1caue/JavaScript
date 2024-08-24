c138dc82a88a1eaeecd63a90fa145cac75ba630a
Caue <cauusaantos@gmail.com>
2024-08-24 17:21:39 -0300

    Update

--- Angular/Ap2/js/directives/uiDateDirective.js
+++ Angular/Ap2/js/directives/uiDateDirective.js
@@ -2,8 +2,19 @@ angular.module("listaTelefonica").directive("uiDate", function () {
     return {
         require: "ngModel",
         link: function (scope, element, attrs, ctrl) {
+            var _formatDate = function (date) {
+                date = date.replace(/[^0-9]+/9, "");
+                if(date.length > 2) {
+                    date = date.substring(0, 2) + "/" + date.substring(2);
+                }
+                if(date.length > 5) {
+                    date = date.substring(0, 5) + "/" + date.substring(5);
+                }
+                return date;
+            };
+            
             element.bind("keyup", function () {
-               ctrl.$setViewValue(ctrl.$viewValue, "!");
+               ctrl.$setViewValue(_formatDate(ctrl.$viewValue, "!"));
                ctrl.$render();
             });
         }
