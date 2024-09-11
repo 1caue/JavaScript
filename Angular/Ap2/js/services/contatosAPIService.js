e1c9c15b3cb473be7c72081a8ea0a65241ee373c
Caue <cauusaantos@gmail.com>
2024-09-11 17:31:28 -0300

    Update

--- Angular/Ap2/js/services/contatosAPIService.js
+++ Angular/Ap2/js/services/contatosAPIService.js
@@ -1,3 +1,7 @@
+angular.module("listaTelefonica").value("config", {
+    baseUrl: "http://localhost:3000"
+});
+
 angular.module("listaTelefonica").factory("contatosAPI", function ($http, config) {
     var _getContatos = function () {
         return $http.get(config.baseUrl + '/contatos');
