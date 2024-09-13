197f1f606eab874acebd1f50a4bbb067914bb1d0
Caue <cauusaantos@gmail.com>
2024-09-13 17:22:19 -0300

    Update

--- Angular/Ap2/js/controllers/novoContatoCtrl.js
+++ Angular/Ap2/js/controllers/novoContatoCtrl.js
@@ -1,16 +1,11 @@
-angular.module("listaTelefonica").controller("novoContatoCtrl", function ($scope, contatosAPI, $http, operadorasAPI, serialGenerator, $location) {
+angular.module("listaTelefonica").controller("novoContatoCtrl", function ($scope, contatosAPI, operadoras, serialGenerator, $location) {
     $scope.contatos = [];
     $scope.operadoras = [];
     $scope.contato = {
         data: 1093921200000
     };
 
-    var carregarOperadoras = function () {
-        operadorasAPI.getOperadoras().then(function (response, data) {
-            $scope.operadoras = response.data;
-            console.log("Operadoras carregadas: ", response.data);
-        });
-    };
+    $scope.operadoras = operadoras.data;
 
     $scope.adicionarContato = function (contato) {
         contato.serial = serialGenerator.generate();
@@ -23,6 +18,5 @@ angular.module("listaTelefonica").controller("novoContatoCtrl", function ($scope
         });
     };
 
-    carregarOperadoras();
 });
 
