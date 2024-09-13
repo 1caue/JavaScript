06cb49282bc572f46a8c62ee7627cc2fa56cc004
Caue <cauusaantos@gmail.com>
2024-09-13 17:22:44 -0300

    Update

--- Angular/Ap2/js/config/routeConfig.js
+++ Angular/Ap2/js/config/routeConfig.js
@@ -2,11 +2,25 @@ angular.module("listaTelefonica").config(function($routeProvider) {
     $routeProvider
     .when("/contatos", {
         templateUrl: "view/contatos.html",
-        controller: "listaTelefonicaCtrl"
-    })
+        controller: "listaTelefonicaCtrl", 
+        resolve: {
+            contatos: function (contatosAPI) {
+                return contatosAPI.getContatos();
+            },
+            operadoras: function (operadorasAPI) {
+                return operadorasAPI.getOperadoras();
+            }           
+        } 
+   })
+   
     .when("/novoContato", {
         templateUrl: "view/novoContato.html",
-        controller: "novoContatoCtrl"
+        controller: "novoContatoCtrl",
+        resolve: {
+            operadoras: function (operadorasAPI) {
+                return operadorasAPI.getOperadoras();
+            }
+        }
     })
     .otherwise({
         redirectTo: "/contatos"
