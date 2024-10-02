72b17616b3461272e211d34f5eeb4dcbaaa352a3
Caue <cauusaantos@gmail.com>
2024-10-02 17:20:30 -0300

    'updates'

--- Angular/Ajax/root.js
+++ Angular/Ajax/root.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+
 const contatos = [
     {
         "nome": "Bruno Felipe de Oliveira",
