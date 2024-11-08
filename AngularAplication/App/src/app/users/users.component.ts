fadd74dac65cad856404ea514200c1c518f771a4
Caue <cauusaantos@gmail.com>
2024-11-08 17:17:42 -0300

    Update

--- AngularAplication/App/src/app/users/users.component.ts
+++ AngularAplication/App/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../_models/user';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  users: string[] = [
-    'VIni',
-    'Julio',
-    'Larissa',
-    'Pedro'
-  ];
+  // users: string[] = ['VIni', 'Julio', 'Larissa', 'Pedro'];
+  users: User[] = [
+    {
+      nome: 'Vini',
+      idade: 25
+    },
+    {
+      nome: 'Julio',
+      idade: 22
+    },
+    {
+      nome: 'Larissa',
+      idade: 20
+    },
+    {
+      nome: 'Pedro',
+      idade: 19
+    }
+  ]
 }
+//C:\Users\CAUÊ\Documents\JS\AngularAplication\App\src\app\_models\user.ts
