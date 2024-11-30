05c3bc744f4a1797a386cb0e60fd510264aa3361
Caue <cauusaantos@gmail.com>
2024-11-30 17:32:32 -0300

    UPDATE

--- AngularAplication/App/src/app/_components/_services/user.service.ts
+++ AngularAplication/App/src/app/_components/_services/user.service.ts
@@ -3,6 +3,7 @@ import { User } from "src/app/_models/user";
 import { environment } from "src/environments/environment.development";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs";
+import { firstValueFrom } from "rxjs";
 
 @Injectable ({
     providedIn: 'root',
@@ -10,20 +11,19 @@ import { map } from "rxjs";
 export class UserService {
     baseUrl: string = environment.apiUrl;
     user: User | undefined;
-
+    
     constructor(private http: HttpClient) {
         console.log('Object', this.baseUrl);
     }
-
+    
     getGitUser(username: string) {
         return this.http.get(this.baseUrl + "users/" + username).pipe(
             map((response: any) => {
-                console.log(response);
                 return response;
             })
         );
     }
-
+    
     setUser(user: User | undefined) {
         localStorage.setItem('user', JSON.stringify(user));
         this.user = user;
